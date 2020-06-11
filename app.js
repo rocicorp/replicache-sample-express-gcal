b70b6106cc85cfd97cc1dd74acd5421416e3f8fa
Aaron Boodman <aaron@aaronboodman.com>
2020-06-11 11:39:14 -1000

    First cut at /replicache-client-view

--- app.js
+++ app.js
@@ -1,7 +1,86 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+const status = require('http-status-codes');
+const { getMaxListeners } = require('process');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/replicache-client-view', async (req, res) => {
+    try {
+        const auth = req.header("Authorization");
+        const calendars = (await gcal('/users/me/calendarList', auth)).items;
+        console.log(`got ${calendars.length} calendars`);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+        const events = [];
+        (await Promise.all(calendars.map(async c => {
+            console.log('Getting events for', c.id);
+            return await getEvents(c.id, auth);
+        }))).forEach(r => events.push(...r));
+
+        const out = {
+            lastMutationID: 0,
+            clientView: {},
+        };
+        for (let e of events) {
+            out.clientView[`/event/${e.id}`] = e;
+        }
+
+        res.json(out);
+    } catch (e) {
+        if (e instanceof UnauthorizedError) {
+            res.status(status.UNAUTHORIZED).end();
+            return;
+        }
+        res.status(status.INTERNAL_SERVER_ERROR).send(e.toString());
+        return;
+    }
+});
+
+async function getEvents(calendarID, auth) {
+    const events = [];
+    let nextPageToken;
+
+    do {
+        const response = await gcal(
+            `/calendars/${encodeURIComponent(calendarID)}/events`,
+            auth,
+            {
+                maxResults: 2500,
+                pageToken: nextPageToken,
+            });
+        events.push(...response.items);
+        nextPageToken = response.nextPageToken;
+    } while (nextPageToken);
+
+    return events;
+}
+
+async function gcal(path, auth, qs) {
+    const req = new fetch.Request(
+        `https://www.googleapis.com/calendar/v3${path}?${querystring.stringify(qs)}`,
+        {
+            headers: {
+                Authorization: auth,
+            },
+        },
+    )
+    console.log('Sending request', req);
+    const resp = await fetch(req);
+    console.log('Got response', resp);
+    if (resp.status == status.UNAUTHORIZED) {
+        throw new UnauthorizedError(resp.url, await resp.text());
+    }
+    if (resp.status != status.OK) {
+        throw new Error(`Unexpected resp from gcal api: url: ${resp.url}: ${resp.status}: ${await resp.text()}`);
+    }
+    return await resp.json();
+}
+
+class UnauthorizedError extends Error {
+    constructor(url, message) {
+        super(`Access to ${url} unauthorized: ${message}`);
+    }
+}
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
