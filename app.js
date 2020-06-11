5ff264c96727f64443317fd3b4a43df4baac632a
Aaron Boodman <aaron@aaronboodman.com>
2020-06-11 11:44:25 -1000

    Remove extraneous import

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fetch = require('node-fetch');
 const querystring = require('querystring');
 const status = require('http-status-codes');
-const { getMaxListeners } = require('process');
 const app = express();
 const port = 3000;
 
